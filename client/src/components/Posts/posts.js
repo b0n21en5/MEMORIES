1d9a426a66fe35cb6b1838d573772e82e8e0ff21
Bikash Nath <splittergame91@gmail.com>
2022-12-21 22:52:48 +0530

    grid

--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -2,19 +2,19 @@ import React from "react";
 import Post from "./Post/Post";
 import useStyles from './styles'
 import {useSelector} from 'react-redux';
+import { Grid, CircularProgress } from "@material-ui/core";
 
 const Posts = () =>{
     const posts = useSelector((state) => state.posts);
-    // eslint-disable-next-line no-unused-vars
     const classes = useStyles();
 
     console.log(posts);
     return (
-        <>
-        <h1>POSTS</h1>
-        <Post/>
-        <Post/>
-        </>
+        !posts.length ? <CircularProgress/> : (
+            <Grid>
+                
+            </Grid>
+        )
     );
 }
 
