da254e3bbca6ecdf8c24253791c735011c94f4ce
Bikash Nath <splittergame91@gmail.com>
2022-12-23 22:02:07 +0530

    dotenv installed

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import  express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
@@ -14,10 +15,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://splitter:Bn91596665@cluster0.p0b80aj.mongodb.net/?retryWrites=true&w=majority';
+
+dotenv.config();
+// const CONNECTION_URL = 'mongodb+srv://splitter:Bn91596665@cluster0.p0b80aj.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
 .then(() =>app.listen(PORT, () => console.log(`Server is running on ${PORT}`)))
 .catch((error) => console.log(error.message));
 
